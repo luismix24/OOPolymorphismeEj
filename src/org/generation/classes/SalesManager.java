f8a24996b9d09dd0d8a6dd3a5df015ef6dae2be7
luisarturo <luisarturoramirez2017@gmail.com>
2024-09-23 09:44:13 -0600

    Se agrega código

--- src/org/generation/classes/SalesManager.java
+++ src/org/generation/classes/SalesManager.java
@@ -1,5 +1,7 @@
 package org.generation.classes;
 
 public class SalesManager {
+	
+	HashMap<int, SalesRep> salesTeam;
 
 }
--- src/org/generation/classes/SalesRep.java
+++ src/org/generation/classes/SalesRep.java
@@ -1,5 +1,15 @@
 package org.generation.classes;
 
 public class SalesRep {
+	double salesMade;
+
+	public SalesRep(double salesMade) {
+		super();
+		this.salesMade = salesMade;
+	}//constructor
+	
+	   public double calculateComission(){
+		   return 0.1 * salesMade;
+		   }
 
 }
