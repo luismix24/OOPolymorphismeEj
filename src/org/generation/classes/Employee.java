7b1f42402083ce640545323691b254d23da145eb
luisarturo <luisarturoramirez2017@gmail.com>
2024-09-23 09:36:14 -0600

    Se agrega código a Employee.java

--- src/org/generation/classes/Employee.java
+++ src/org/generation/classes/Employee.java
@@ -1,5 +1,102 @@
 package org.generation.classes;
 
 public class Employee {
+	private String firsName;
+	private String lastName;
+	private int registration;
+	private int age;
+	private int daysWorked;
+	private int vacationDaysTaken;
+	private double salary;
+	private int yearsWorked;
+	
+	public Employee(String firsName, String lastName, int registration, int age, int daysWorked, int vacationDaysTaken,
+			double salary, int yearsWorked) {
+		super();
+		this.firsName = firsName;
+		this.lastName = lastName;
+		this.registration = registration;
+		this.age = age;
+		this.daysWorked = daysWorked;
+		this.vacationDaysTaken = vacationDaysTaken;
+		this.salary = salary;
+		this.yearsWorked = yearsWorked;
+	}//constructor
+	
+	   public String getFirsName() {
+		return firsName;
+	}
 
-}
+	public void setFirsName(String firsName) {
+		this.firsName = firsName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(int registration) {
+		this.registration = registration;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getDaysWorked() {
+		return daysWorked;
+	}
+	
+	public void setDaysWorked(int daysWorked) {
+		this.daysWorked = daysWorked;
+	}
+
+	public int getVacationDaysTaken() {
+		return vacationDaysTaken;
+	}
+
+	public void setVacationDaysTaken(int vacationDaysTaken) {
+		this.vacationDaysTaken = vacationDaysTaken;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getYearsWorked() {
+		return yearsWorked;
+	}
+
+	public void setYearsWorked(int yearsWorked) {
+		this.yearsWorked = yearsWorked;
+	}
+
+	public int timeToRetirement(){
+		 return Math.min(60 - age, 40 - yearsWorked);
+		   }
+
+		   public int vacationTimeLeft(){
+			   return (int)((daysWorked / 360.0) * (30 - vacationDaysTaken));
+		   }
+
+		   public double calculateBonus(){
+			   return 2.2 * salary;
+		   }
+
+}//class Employee
